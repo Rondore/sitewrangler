68f92f0473526e6bb91a91b382c1b2d1340a14f5
Rondore <nipitmaster@gmail.com>
2020-07-22 22:33:37 -0600

    Correct spelling of image_magick package

--- bin/builders/php_builder.py
+++ bin/builders/php_builder.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 
-from libsw import build_index, curl, openssl, php, postgresql, image_magic, pecl_imagick, pecl_memcached, pecl_redis
+from libsw import build_index, curl, openssl, php, postgresql, image_magick, pecl_imagick, pecl_memcached, pecl_redis
 
 index = build_index.Index()
 index.register_builder( openssl.OpensslBuilder() )
 index.register_builder( curl.CurlBuilder() )
 index.register_builder( postgresql.PostgresqlBuilder() )
 index.register_builder( php.ImapBuilder() )
-index.register_builder( image_magic.ImageMagickBuilder() )
+index.register_builder( image_magick.ImageMagickBuilder() )
 index.register_builder( pecl_imagick.ImagickBuilder() )
 index.register_builder( pecl_memcached.MemcachedBuilder() )
 index.register_builder( pecl_redis.RedisBuilder() )
