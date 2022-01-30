7c8d78976d234e78a20405008ee45d6d106b3f15
Rondore <nipitmaster@gmail.com>
2022-01-29 22:32:40 -0600

    Add documentation to self_update function

--- bin/libsw/recursion.py
+++ bin/libsw/recursion.py
@@ -4,6 +4,9 @@ import subprocess
 from libsw import logger, settings
 
 def self_update():
+    '''
+    Update Site Wrangler to the latest version
+    '''
     install_path = settings.get('install_path')
     log = logger.Log()
     command = [
