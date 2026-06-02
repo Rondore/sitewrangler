785a8194f446a1836608cd230ec7fce8815aaffe
Rondore <nipitmaster@gmail.com>
2026-06-02 00:43:05 -0700

    Add container compose files

--- bin/libsw/compose.py
+++ bin/libsw/compose.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import subprocess
 import os
 from libsw import settings, template
 
@@ -14,3 +15,29 @@ def write_compose(filename: str):
     target_file = compose_dir() + filename
     template_vars = template.get_template_vars()
     template.write_template_with_variables(source_file, target_file, template_vars)
+
+def get_compose_file_args(compose_file: str | False = False):
+    compose_list = []
+    parent_dir = compose_dir()
+    if(compose_file):
+        compose_list = ['-f', compose_file]
+    else:
+        for file in os.listdir(parent_dir):
+            extention = file.lower().split('.')[-1]
+            if extention in ['yaml', 'yml']:
+                compose_list.extend(['-f', parent_dir + file])
+    return compose_list
+
+def compose_up(compose_file: str | False = False):
+    command: list[str] = [settings.get('build_system')]
+    command.extend(['compose', 'up', '-d'])
+    command.extend(get_compose_file_args(compose_file))
+    output = subprocess.getoutput(command)
+    return output
+
+def compose_down(compose_file: str | False = False):
+    command: list[str] = [settings.get('build_system')]
+    command.extend(['compose', 'down'])
+    command.extend(get_compose_file_args(compose_file))
+    output = subprocess.getoutput(command)
+    return output
